42360c46a00bd51ee9610b399c7cb647496f5725
swon3210 <swon3210@gmail.com>
2020-04-15 13:52:07 +0900

    page renamed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+// PAGES
+import ZoneSearchPage from './pages/ZoneSearchPage/ZoneSearchPage';
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +13,7 @@ function App() {
       </header>
       <main>
         <BrowserRouter>
-          <Route path="/search" exact>searchPage</Route>
+          <Route path="/search" exact component={ZoneSearchPage}>searchPage</Route>
         </BrowserRouter>
       </main>
       <footer>
