25c1844b1447a90ce938458f3d1cb3f687076319
Youngsdg1 <52898735+Youngsdg1@users.noreply.github.com>
2020-04-24 01:48:40 +0900

    update layout && current location

--- src/pages/ZoneSearchPage/ZoneSearchPage.tsx
+++ src/pages/ZoneSearchPage/ZoneSearchPage.tsx
@@ -3,15 +3,29 @@ import DaumPostcode from 'react-daum-postcode';
 import './ZoneSearchPage.scss';
 import ZoneSearchPopUp from './ZoneSearchPopup';
 
+
 const ZoneSearchPage = () => {
-  const ZoneSearchClickHandler = () => {
-    console.log('hit')
+  const onClickLocationHandler = () => {
+    console.log('hit')  // ZoneSearchPopUp 팝업 창 띄우기
   }
   return (
-    <>
-      {/* <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js"></script> */}
+    <div className="search1_select_done">
       {/* <input type="text" onChange={ZoneSearchClickHandler} value="주소를 입력하세요" /> */}
-      <ZoneSearchPopUp></ZoneSearchPopUp>
+      {/* <ZoneSearchPopUp></ZoneSearchPopUp> */}
+      <span className="STEP-1">STEP 1</span>
+      <span className="location"> / location</span>
+      <br />
+      <span className="Rectangle_ment">평소 자주 방문하는 곳의</span>
+      <br />
+      <span className="Rectangle_ment">위치를 알려주세요</span>
+      <br />
+      <span className="Rectangle_ment">해시태그부분</span>
+      <br />
+      <input type="text" placeholder="주소를 입력하세요" onClick={onClickLocationHandler} className="Rectangle_Long">
+      </input>
+      <input type="button" className="Rectangle_location"/>
+      <span className="Rectangle_ment">Footer 부분</span>
+
       {/* <script>
         new daum.Postcode({
           oncomplete: function(data) {
@@ -20,7 +34,7 @@ const ZoneSearchPage = () => {
         }
         }).open();
       </script> */}
-    </>
+    </div>
   )
 }
 
