4d02bfc19969bd1d67a562005ae1dbca895c766d
Youngsdg1 <52898735+Youngsdg1@users.noreply.github.com>
2020-04-27 00:59:42 +0900

    update hashtag

---
+++ src/pages/ZoneSearchPage/HashTag.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './ZoneSearchPage.scss';
+
+const HashTag = () => {
+  const tagList: any = ['#회사', '#학교', '추가+']
+
+  const addTag: any = (array: Array<string>, index: number, newItem: string) => {
+    return [
+      ...array.slice(0, index),
+      newItem,
+      ...array.slice(index)
+    ];
+  }
+
+  const onClickHandler = (e: any) => {
+    return e === '추가+' ? addTag(tagList, -1, e) : e
+  }
+
+  const tagListMap: any = tagList.map((e: any, idx: number) => {
+    return (
+      <>
+        {e}
+        <div className="StyledHashTag"
+          key={idx}
+          onClick={() => onClickHandler(e)}
+        />
+      </>
+    )
+  })
+  return (
+    <>
+      {tagListMap}
+    </>
+  )
+}
+
+export { HashTag };
