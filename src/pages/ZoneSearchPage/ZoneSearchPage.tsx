0e74120fb59e3556ccc45ca5eb07341a268152fa
Otterji <youngsdg1@gmail.com>
2020-05-02 16:39:46 +0900

    zonesearch

--- src/pages/ZoneSearchPage/ZoneSearchPage.tsx
+++ src/pages/ZoneSearchPage/ZoneSearchPage.tsx
@@ -3,12 +3,22 @@ import DaumPostcode from 'react-daum-postcode';
 import './ZoneSearchPage.scss';
 import ZoneSearchPopUp from './ZoneSearchPopup';
 
-
 const ZoneSearchPage = () => {
   const onClickLocationHandler = () => {
-    console.log('hit')  // ZoneSearchPopUp 팝업 창 띄우기
-  }
+    console.log('hit'); // ZoneSearchPopUp 팝업 창 띄우기
+    return (
+      <>
+        {/* // eslint-disable-next-line @typescript-eslint/no-unused-expressions */}
+        <ZoneSearchPopUp />
+      </>
+    );
+  };
   return (
+    // <>
+    // <ZoneSearchPopUp></ZoneSearchPopUp>
+    // <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js?autoload=false"></script>
+    // </>
+
     <div className="search1_select_done">
       {/* <input type="text" onChange={ZoneSearchClickHandler} value="주소를 입력하세요" /> */}
       {/* <ZoneSearchPopUp></ZoneSearchPopUp> */}
@@ -21,21 +31,16 @@ const ZoneSearchPage = () => {
       <br />
       <span className="Rectangle_ment">해시태그부분</span>
       <br />
-      <input type="text" placeholder="주소를 입력하세요" onClick={onClickLocationHandler} className="Rectangle_Long">
-      </input>
-      <input type="button" className="Rectangle_location"/>
+      <input
+        type="text"
+        placeholder="주소를 입력하세요"
+        onClick={onClickLocationHandler}
+        className="Rectangle_Long"
+      ></input>
+      <input type="button" className="Rectangle_location" />
       <span className="Rectangle_ment">Footer 부분</span>
-
-      {/* <script>
-        new daum.Postcode({
-          oncomplete: function(data) {
-          // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
-          // 예제를 참고하여 다양한 활용법을 확인해 보세요.
-        }
-        }).open();
-      </script> */}
     </div>
-  )
-}
+  );
+};
 
 export default ZoneSearchPage;
--- src/pages/ZoneSearchPage/ZoneSearchPopup.tsx
+++ src/pages/ZoneSearchPage/ZoneSearchPopup.tsx
@@ -6,27 +6,27 @@ const ZoneSearchPopUp = () => {
   const handleComplete = (data: any) => {
     let fullAddress = data.address;
     let extraAddress = '';
-    
+
     if (data.addressType === 'R') {
       if (data.bname !== '') {
         extraAddress += data.bname;
       }
       if (data.buildingName !== '') {
-        extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+        extraAddress +=
+          extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName;
       }
-      fullAddress += (extraAddress !== '' ? ` (${extraAddress})` : '');
+      fullAddress += extraAddress !== '' ? ` (${extraAddress})` : '';
     }
 
-    alert(fullAddress);  // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
-  }
+    alert(fullAddress); // e.g. '서울 성동구 왕십리로2길 20 (성수동1가)'
+  };
 
   return (
     <>
-      <DaumPostcode
-        onComplete={handleComplete}
-      />
+      <DaumPostcode onComplete={handleComplete} />
+      <script src="http://dmaps.daum.net/map_js_init/postcode.v2.js?autoload=false"></script>
     </>
   );
-}
+};
 
 export default ZoneSearchPopUp;
