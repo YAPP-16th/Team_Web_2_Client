cf3edd4cac67293de90b6f44cdd1094d5c04fbca
swon3210 <swon3210@gmail.com>
2020-05-02 15:16:24 +0900

    EDIT realestate test

--- src/containers/RealEstateContainer/RealEstateContainer.test.tsx
+++ src/containers/RealEstateContainer/RealEstateContainer.test.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import RealEstateContainer from './RealEstateContainer';
+import { Router } from "react-router-dom";
+
+import { createMemoryHistory } from "history";
 
 describe("<RealEstateContainer />", () => {
-  it("매물 데이터 가져오기", () => {
-    const utils = render(<RealEstateContainer />);
-  });
-  it("동적 스크롤 기능", () => {
-    const utils = render(<RealEstateContainer />);
-  });
+  it("매물")
+  // it("매물 데이터 가져오기", () => {
+  //   const utils = render(<RealEstateContainer />);
+  // });
+  // it("동적 스크롤 기능", () => {
+  //   const utils = render(<RealEstateContainer />);
+  // });
 });
