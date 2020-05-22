ef10d0b7d07ef9f997463280564cfe764b2cac2b
AnGwangHo <akh4444@nate.com>
2020-05-23 03:45:10 +0900

    - 불필요한 style 제거

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -14,8 +14,6 @@ const DEVICE_SIZE = {
 
 const ToolbarWrapper = styled.nav`
   padding: 44px 33px 26px;
-  backgroundcolor: var(--BackgroundColor);
-  color: var(--GreyTextColor);
 
   @media only screen and (max-width: ${DEVICE_SIZE.mobile}) {
     padding: 18px 20px;
