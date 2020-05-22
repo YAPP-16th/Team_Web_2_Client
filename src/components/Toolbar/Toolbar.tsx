b50a67763c09dc480747ef685050abd4ef2ffe06
AnGwangHo <akh4444@nate.com>
2020-05-23 03:19:30 +0900

    - Nav사용 시 ul-li 구조 사용하도록 DOM변경 - 여백용 DOM 대신 flex사용하여 style 설정 - 반응형 작업

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,45 +1,44 @@
 import React from "react";
 import styled from "styled-components";
-import { FlexBoxSpacer } from "../../utils/utilComponents";
-
-// Icon
-import Icon from '../Icon/Icon';
 
 type ToolbarProps = {
   rightContents?: React.ReactNode;
   leftContents?: React.ReactNode;
 };
 
-const ToolbarWrapper = styled.header`
-  padding: 18px 20px;
-  backgroundColor: var(--BackgroundColor);
+const DEVICE_SIZE = {
+  mobile: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+};
+
+const ToolbarWrapper = styled.nav`
+  padding: 44px 33px 26px;
+  backgroundcolor: var(--BackgroundColor);
   color: var(--GreyTextColor);
+
+  @media only screen and (max-width: ${DEVICE_SIZE.mobile}) {
+    padding: 18px 20px;
+  }
 `;
 
-const ToolbarContent = styled.nav`
+const ToolbarContent = styled.ul`
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
-const ToolbarLeftItem = styled.div`
-
-`;
-
-const ToolbarRightItem = styled.div`
-
+const ToolbarItem = styled.li`
+  list-style: none;
+  padding-left: 0px;
 `;
 
 const Toolbar = ({ rightContents, leftContents }: ToolbarProps) => {
   return (
     <ToolbarWrapper>
       <ToolbarContent>
-        <ToolbarLeftItem>
-          {leftContents}
-        </ToolbarLeftItem>
-        <FlexBoxSpacer />
-        <ToolbarRightItem>
-          {rightContents}
-        </ToolbarRightItem>
+        <ToolbarItem>{leftContents}</ToolbarItem>
+        <ToolbarItem>{rightContents}</ToolbarItem>
       </ToolbarContent>
     </ToolbarWrapper>
   );
