04825cf28660ff285689f6971bc1a12b07eeb85b
AnGwangHo <akh4444@nate.com>
2020-05-22 23:36:48 +0900

    - rootAPI 생성 - zoneAPI 생성 - rootAPI통해서 사용하도록 기존 rooms 파일 수정

--- src/api/rooms.ts
+++ src/api/rooms.ts
@@ -1,23 +1,23 @@
-import axios from 'axios';
+import * as RequestAPI from "./rootAPI";
 
 export type Room = {
   name: string;
   description: string;
-  coordinate: { latitude: number, longitude: number };
+  coordinate: { latitude: number; longitude: number };
   buildingType: "빌라" | "주택" | "아파트";
   livingType: "월세" | "전세" | "하숙?";
   monthlyPayment: number;
   deposit: number;
-}
+};
 
-export type Zone = { 
-  latitude: number; 
-  longitude: number; 
-  radius: number; 
-}
+export type Zone = {
+  latitude: number;
+  longitude: number;
+  radius: number;
+};
 
-export async function getRooms (payload: Zone) {
-  const response = await axios.get<Room[]>(`url`);  
+export async function getRooms(payload: Zone) {
+  const url = "url";
+  const response = await RequestAPI.get({ url, data: payload });
   return response.data;
 }
-
