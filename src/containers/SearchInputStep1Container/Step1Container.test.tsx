98e65091ddf9ab40c7c56b449965a104c3ec41af
youngji <otterji@pulitsoft.com>
2020-05-23 16:38:58 +0900

    테스트코드 2

--- src/containers/SearchInputStep1Container/Step1Container.test.tsx
+++ src/containers/SearchInputStep1Container/Step1Container.test.tsx
@@ -1,47 +1,79 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Step1Container from "./Step1Container";
-import Step2Container from '../SearchInputStep2Container/Step2Container'
 
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import rootReducer from "../../modules";
-import { Router } from "react-router-dom";
+const DEFAULT_VALUES = {
+  searchInputData: {
+    address: '주소를 입력해 주세요',
+    addressTag: 'tag',
+    maxTime: 0,
+    minTime: 0,
+    transferLimit: 100,
+    transitMode: [],
+    // setIsHover: 
+  }
+};
 
-import { createMemoryHistory } from "history";
-
-const store = createStore(rootReducer);
-
-describe("<Step1Container />", () => {
-  it("드래그 동작에 의한 애니메이션 처리", () => {
-    const history = createMemoryHistory();
-    history.push("/searchInput/1");
-    const utils = render(
-      <Provider store={store}>
-        <Router history={history}>
-          <Step1Container />
-        </Router>
-      </Provider>
-    );
-
-    // const toggleButton = utils.getByText("리스트뷰 +");
-    // fireEvent.click(toggleButton);
-    // utils.findByText("리스트뷰 -")
-    // .then((element) => {
-    //   console.log("element.clientTop", element.clientTop);
-    //   // expect(element.clientTop).toBe('리스트뷰')
-    // });
+describe("Step1Container 컴포넌트", () => {
+  describe("import 했을 시", () => {
+    it("정상적으로 Load 되었다.", () => {
+      expect(Step1Container).not.toBeNull();
+      expect(Step1Container).not.toBeUndefined();
+      expect(Step1Container).toBeDefined();
+    });
   });
 
-  it("다음으로 클릭 시 리덕스 업데이트 후 다음 페이지로 이동", () => {
-    const history = createMemoryHistory();
-    history.push("/searchInput/2");
-    const utils = render(
-      <Provider store={store}>
-        <Router history={history}>
-          <Step2Container />
-        </Router>
-      </Provider>
-    );
+  describe("사용 했을 때", () => {
+    it("사용자 설정값이 정상적으로 설정 되었다.", () => {
+      const TAG_NAME = ["svg", "DIV", "BUTTON", "DIV"];
+      const MATCH_STRING: string[] = [
+        "loading-twinkle.svg",
+        "icon-close.svg",
+        "",
+        "loadingSpinner",
+      ];
+      const { container } = render(
+        <Step1Container
+          setIsHover={DEFAULT_VALUES.setIsHover}
+        />
+      );
+
+      expect(container.innerHTML).not.toEqual("");
+      expect(container.innerHTML).toMatch(`id="` + DEFAULT_VALUES.id + `"`);
+
+      for (let i = 0; i < container.children.length; i++) {
+        const { children } = container.children[i];
+        expect(container.children[i].tagName).toEqual("DIV");
+        expect(container.children[i].children.length).toBe(4);
+
+        for (let j = 0; j < children.length; j++) {
+          expect(children[j].tagName).toEqual(TAG_NAME[j]);
+          if (j === 1) {
+            expect(children[j].children.length).toBe(7);
+            const content = children[j].children as HTMLCollection;
+            let idx = 0;
+            const TEST_VALUES = [
+              DEFAULT_VALUES.address,
+              DEFAULT_VALUES.transitMode,
+              `${DEFAULT_VALUES.minTime}분-${DEFAULT_VALUES.maxTime}분 이내`,
+            ];
+
+            for (let element of content) {
+              if (element.tagName === "SPAN") {
+                expect(element.innerHTML).toEqual(TEST_VALUES[idx]);
+                idx++;
+              }
+            }
+          } else {
+            expect(children[j].innerHTML).toMatch(MATCH_STRING[j]);
+
+            if (children[j].tagName === "BUTTON") {
+              const Button = children[j] as HTMLElement;
+              expect(Button.onclick).toBeDefined();
+            }
+          }
+        }
+      }
+    });
   });
 });
