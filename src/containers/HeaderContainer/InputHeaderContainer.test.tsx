b6afa497ec99cf7be37bf1640b07f64e9cd98808
youngji <otterji@pulitsoft.com>
2020-06-15 21:36:32 +0900

    test 코드 삭제

--- src/containers/HeaderContainer/InputHeaderContainer.test.tsx
+++ src/containers/HeaderContainer/InputHeaderContainer.test.tsx
@@ -11,18 +11,18 @@ import InputHeaderContainer from "./InputHeaderContainer";
 import DefaultHeaderContainer from "./DefaultHeaderContainer";
 import modalHooks from "../../components/Modal/ModalHooks";
 
-describe("<InputHeaderContainer />", () => {
-  const store = createStore(rootReducer);
+// describe("<InputHeaderContainer />", () => {
+//   const store = createStore(rootReducer);
 
-  // it("홈으로 이동하기(랜딩 페이지)", () => {
-  //   const history = createMemoryHistory();
-  //   history.push("/");
-  //   const utils = render(
-  //     <Provider store={store}>
-  //       <Router history={history}>
-  //         <DefaultHeaderContainer />
-  //       </Router>
-  //     </Provider>
-  //   );
-  // });
-});
+//   // it("홈으로 이동하기(랜딩 페이지)", () => {
+//   //   const history = createMemoryHistory();
+//   //   history.push("/");
+//   //   const utils = render(
+//   //     <Provider store={store}>
+//   //       <Router history={history}>
+//   //         <DefaultHeaderContainer />
+//   //       </Router>
+//   //     </Provider>
+//   //   );
+//   // });
+// });
